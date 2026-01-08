aa9fc6fad44125e0cd327d629f90f766ede98b84
GerhardAhrens <developer@lifeprojects.de>
2026-01-08 14:15:21 +0100

    Weitere Funktionen zur Monitorermittlung

---
+++ WPFMonitorKontrolle/MonitorTool.cs
@@ -0,0 +1,189 @@
+﻿namespace WPFMonitorKontrolle
+{
+    using System.Management;
+    using System.Runtime.InteropServices;
+
+    public class MonitorTool
+    {
+
+        public static List<WmiMonitor> GetMonitorNames()
+        {
+            var list = new List<WmiMonitor>();
+            var searcher = new ManagementObjectSearcher(@"root\wmi", "SELECT * FROM WmiMonitorID");
+
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                list.Add(new WmiMonitor
+                {
+                    InstanceName = obj["InstanceName"].ToString(),
+                    Manufacturer = Decode((ushort[])obj["ManufacturerName"]),
+                    Model = Decode((ushort[])obj["UserFriendlyName"]),
+                    Serial = Decode((ushort[])obj["SerialNumberID"]),
+                });
+            }
+
+            if (list.Count == 1)
+            {
+                list[0].InstanceName = list[0].InstanceName.Replace("DISPLAY","DISPLAY1");
+            }
+
+            return list;
+        }
+
+        public static List<MonitorInfo> GetMonitors()
+        {
+            var result = new List<MonitorInfo>();
+
+            var screens = Screen.AllScreens;
+            var wmi = GetMonitorNames();
+
+            for (int i = 0; i < screens.Length; i++)
+            {
+                var screen = screens[i];
+
+                var hMonitor = NativeMethods.GetMonitorHandle(screen);
+                uint dpiX = 0, dpiY = 0;
+                int hr = NativeMethods.GetDpiForMonitor(hMonitor, MonitorDpiType.EffectiveDpi, out dpiX, out dpiY);
+
+                if (hr != 0)
+                {
+                    // Fehler beim Abrufen der DPI, Standardwerte verwenden
+                    dpiX = 96;
+                    dpiY = 96;
+                }
+
+                var wmiMonitor = i < wmi.Count ? wmi[i] : null;
+
+                result.Add(new MonitorInfo
+                {
+                    DisplayName = screen.DeviceName,
+                    IsPrimary = screen.Primary,
+
+                    Manufacturer = wmiMonitor?.Manufacturer ?? "Unbekannt",
+                    Model = wmiMonitor?.Model ?? "Unbekannt",
+                    Serial = wmiMonitor?.Serial ?? "Unbekannt",
+
+                    DpiX = (int)dpiX,
+                    DpiY = (int)dpiY,
+                    X = screen.Bounds.Left,
+                    Y = screen.Bounds.Top,
+                    Width = screen.Bounds.Width,
+                    Height = screen.Bounds.Height,
+                    PositionDescription = GetPositionDescription(screen)
+                });
+            }
+
+            return result;
+        }
+
+        static string Decode(ushort[] data)
+        {
+            if (data == null)
+            {
+                return string.Empty;
+            }
+
+            var chars = data.TakeWhile(c => c != 0).Select(c => (char)c).ToArray();
+
+            return new string(chars);
+        }
+
+        static string GetPositionDescription(Screen screen)
+        {
+            if (screen.Primary)
+            {
+                return "Primär";
+            }
+
+            if (screen.Bounds.Right <= Screen.PrimaryScreen.Bounds.Left)
+            {
+                return "Links vom Primärmonitor";
+            }
+
+            if (screen.Bounds.Left >= Screen.PrimaryScreen.Bounds.Right)
+            {
+                return "Rechts vom Primärmonitor";
+            }
+
+            if (screen.Bounds.Bottom <= Screen.PrimaryScreen.Bounds.Top)
+            {
+                return "Oberhalb des Primärmonitors";
+            }
+
+            if (screen.Bounds.Top >= Screen.PrimaryScreen.Bounds.Bottom)
+            {
+                return "Unterhalb des Primärmonitors";
+            }
+
+            return "Überlappend / Benutzerdefiniert";
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct DisplayDevice
+    {
+        public int cb;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string DeviceName;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+        public string DeviceString;
+        public int StateFlags;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+        public string DeviceID;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+        public string DeviceKey;
+    }
+
+    public enum MonitorDpiType
+    {
+        EffectiveDpi = 0,
+        AngularDpi = 1,
+        RawDpi = 2
+    }
+
+    public static class NativeMethods
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        internal static extern bool EnumDisplayDevices(string lpDevice, uint iDevNum, ref DisplayDevice lpDisplayDevice, uint dwFlags);
+
+        public static IntPtr GetMonitorHandle(Screen screen)
+        {
+            return MonitorFromPoint(new Point(screen.Bounds.Left + 1,screen.Bounds.Top + 1), 2);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr MonitorFromPoint(Point pt, uint flags);
+
+        [DllImport("shcore.dll", CharSet = CharSet.Unicode)]
+        internal static extern int GetDpiForMonitor(IntPtr hmonitor, MonitorDpiType dpiType, out uint dpiX, out uint dpiY);
+    }
+    
+    public class WmiMonitor
+    {
+        public string InstanceName { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Serial { get; set; }
+
+    }
+
+    public class MonitorInfo
+    {
+        public string DisplayName { get; set; }
+        public bool IsPrimary { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Serial { get; set; }
+
+        public int DpiX { get; set; }
+        public int DpiY { get; set; }
+        public double ScaleFactor => DpiX / 96.0;
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public string PositionDescription { get; set; }
+    }
+}
